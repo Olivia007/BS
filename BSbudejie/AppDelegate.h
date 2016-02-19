595ab314c051061a8c5a5b6b3071b3dea890c110
“Olivia007” <971204826@qq.com>
2016-02-19 14:30:59 +0800

    add code
    create project

---
+++ BSbudejie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BSbudejie
+//
+//  Created by wacosoft on 16/2/19.
+//  Copyright © 2016年 wacosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
