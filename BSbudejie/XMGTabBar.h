19f6f9c8002d50ec850555f0d4bc7383f9efd4bf
“Olivia007” <971204826@qq.com>
2016-02-19 16:53:56 +0800

    5.custom tabbar

---
+++ BSbudejie/XMGTabBar.h
@@ -0,0 +1,13 @@
+//
+//  XMGTabBar.h
+//  BSbudejie
+//
+//  Created by wacosoft on 16/2/19.
+//  Copyright © 2016年 wacosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XMGTabBar : UITabBar
+
+@end
